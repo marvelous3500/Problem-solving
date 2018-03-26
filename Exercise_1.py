b5acf918c2f09eefaee224eb64bf9bca800123b1
vester <you@example.com>
2018-03-26 12:57:44 +0100

    solution of exercise one

---
+++ Exercise_1.py
@@ -0,0 +1,7 @@
+```
+*Exercise 1*
+Create a method that takes an array of numbers and a targeted number and returns similar number pairs that when added gives the same result as the targeted number. 
+_For Example_:  `array = [1,2,3,4,5,6,1,3,2,1,4,5,3,5,2,5]` and `Target = 8` it should 
+`[[5,3],[3,5]]` because when you added them it give `8` which is the target
+
+```
